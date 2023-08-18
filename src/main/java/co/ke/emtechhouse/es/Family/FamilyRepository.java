01b5bf61a7f4e9b6111f6fbae227cbb8e5e7e4e8
Eugineluck <wafulae7.ew@gmail.com>
2023-08-18 15:49:48 +0300

    done

--- src/main/java/co/ke/emtechhouse/es/Family/FamilyRepository.java
+++ src/main/java/co/ke/emtechhouse/es/Family/FamilyRepository.java
@@ -38,6 +38,12 @@ public interface FamilyRepository extends JpaRepository<Family,Long> {
     @Transactional
     @Query(nativeQuery = true, value = "SELECT f.id AS familyId,m.member_role as role, f.family_number AS fn, c.community_name AS communities, f.family_name AS families, o.out_station_name AS church, COUNT(m.id) AS members FROM community c, out_station o, family f LEFT JOIN members m ON f.id = m.family_id WHERE c.out_station_id = o.id AND f.community_id = c.id GROUP BY f.id")
     List<Familydata> getAllFamilies();
+    @Transactional
+    @Query(nativeQuery = true, value = "SELECT f.id AS familyId,m.member_role as role, f.family_number AS fn, c.community_name AS communities,c.id AS communityId, f.family_name AS families, o.out_station_name AS church, COUNT(m.id) AS members FROM community c, out_station o, family f LEFT JOIN members m ON f.id = m.family_id WHERE c.out_station_id = o.id AND f.community_id = c.id  AND  c.id =:communityId GROUP BY f.id")
+    List<Familydata> searchByCommunityId(Long communityId);
+  @Transactional
+    @Query(nativeQuery = true, value = "SELECT f.id AS familyId,m.member_role as role, f.family_number AS fn, c.community_name AS communities,c.id AS communityId, f.family_name AS families, o.out_station_name AS church, COUNT(m.id) AS members FROM community c, out_station o, family f LEFT JOIN members m ON f.id = m.family_id WHERE c.out_station_id = o.id AND f.community_id = c.id  AND  o.id =:churchId GROUP BY f.id")
+    List<Familydata> searchByChurchId(Long churchId);
 
     @Transactional
     @Query(nativeQuery = true,value = "SELECT m.id as id,m.member_role as role,f.id as familyId, m.first_name AS firstname, m.last_name AS lastname, m.member_number AS number,  f.family_name AS family FROM members m JOIN family f on f.id = m.family_id where f.id =:familyId group by m.id")
