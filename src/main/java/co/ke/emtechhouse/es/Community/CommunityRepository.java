54a2e2d642aa5d34b6d2789a72d434092a30db8a
titusmbole <titusmbole9@gmail.com>
2023-08-19 14:11:00 -0700

    Changes

--- src/main/java/co/ke/emtechhouse/es/Community/CommunityRepository.java
+++ src/main/java/co/ke/emtechhouse/es/Community/CommunityRepository.java
@@ -15,5 +15,11 @@ public interface CommunityRepository extends JpaRepository<Community,Long> {
     @Transactional
     @Query(nativeQuery = true,value = "SELECT c.id as communityId, c.community_name AS CommunityName, c.community_location AS location,o.out_station_name as OutstationName,(SELECT COUNT(DISTINCT id) FROM members WHERE c.id = community_id) AS members,(SELECT COUNT(DISTINCT id) FROM family WHERE c.id = community_id) AS families FROM community c, out_station o where c.out_station_id=o.id;  ")
     List<CommunityDetails> getAllCommunity();
+
+    @Transactional
+    @Query(nativeQuery = true,value = "SELECT c.id as communityId, c.community_name AS CommunityName, c.community_location AS location,o.out_station_name as OutstationName,(SELECT COUNT(DISTINCT id) FROM members WHERE c.id = community_id) AS members,(SELECT COUNT(DISTINCT id) FROM family WHERE c.id = community_id) AS families FROM community c, out_station o where c.out_station_id= :id;  ")
+    List<CommunityDetails> getAllCommunitiesOutstation(Long id);
+
+
 }
 //SELECT  c.id as communityId,c.community_name as communities,c.community_location as location,o.out_station_name as station from community c, out_station o where c.out_station_id=o.id"
