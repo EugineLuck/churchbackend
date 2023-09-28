080309a1a39259327b26eb192be9d51ad2a8eda0
Eugineluck <wafulae7.ew@gmail.com>
2023-09-28 12:44:58 +0300

    ok

--- src/main/java/co/ke/emtechhouse/es/Location/County.java
+++ src/main/java/co/ke/emtechhouse/es/Location/County.java
@@ -1,19 +1,19 @@
-package co.ke.emtechhouse.es.Location;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.List;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@ToString
-public class County {
-    private String countyName;
-    private String countyCapital;
-    private String countyCode;
-    private List subCounties;
-}
+//package co.ke.emtechhouse.es.Location;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//import java.util.List;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@ToString
+//public class County {
+//    private String countyName;
+//    private String countyCapital;
+//    private String countyCode;
+//    private List subCounties;
+//}
--- src/main/java/co/ke/emtechhouse/es/Location/LocationController.java
+++ src/main/java/co/ke/emtechhouse/es/Location/LocationController.java
@@ -1,33 +1,33 @@
-package co.ke.emtechhouse.es.Location;
-
-import co.ke.emtechhouse.es.Auth.utils.MigrationData.CountiesJson;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@Slf4j
-@RestController
-@RequestMapping("/getLocations/")
-public class LocationController {
-    public final CountiesJson countiesJson;
-    public LocationController(CountiesJson countiesJson){
-
-        this.countiesJson = countiesJson;
-    }
-
-    @GetMapping("/get/all")
-    public ResponseEntity<Object> getLocatiions() {
-        try {
-            List<County> allCounties = (List<County>) countiesJson;
-            return new ResponseEntity<>(allCounties, HttpStatus.OK);
-        } catch (Exception e) {
-            log.info("Error" + e);
-            return null;
-        }
-    }
-}
+//package co.ke.emtechhouse.es.Location;
+//
+//import co.ke.emtechhouse.es.Auth.utils.MigrationData.CountiesJson;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/getLocations/")
+//public class LocationController {
+//    public  CountiesJson countiesJson;
+//    public LocationController(CountiesJson countiesJson){
+//
+//        this.countiesJson = countiesJson;
+//    }
+//
+//    @GetMapping("/get/all")
+//    public ResponseEntity<Object> getLocatiions() {
+//        try {
+//            List<County> allCounties = (List<County>) countiesJson;
+//            return new ResponseEntity<>(allCounties, HttpStatus.OK);
+//        } catch (Exception e) {
+//            log.info("Error" + e);
+//            return null;
+//        }
+//    }
+//}
