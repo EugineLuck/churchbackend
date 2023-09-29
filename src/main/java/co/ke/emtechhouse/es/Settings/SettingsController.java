0c34879027748fe42459e3f85571584f75ff5903
titusmbole <titusmbole9@gmail.com>
2023-09-29 11:52:24 -0700

    chnages

--- src/main/java/co/ke/emtechhouse/es/Settings/SettingsController.java
+++ src/main/java/co/ke/emtechhouse/es/Settings/SettingsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -42,20 +43,20 @@ public class SettingsController {
         }
     }
 
-//    @PutMapping("/update")
-//    public ResponseEntity<Object> updateSettings(@RequestBody Settings settings) {
-//        ApiResponse response = new ApiResponse<>();
-//        try {
-//            Settings savedSettings = settingsRepository.save(settings);
-//            response.setEntity(savedSettings);
-//            response.setMessage("Settings Saved");
-//            response.setStatusCode(200);
-//            return new ResponseEntity<>(response, HttpStatus.OK);
-//        } catch (Exception e) {
-//            log.info("Error" + e);
-//            return null;
-//        }
-//    }
+    @GetMapping("/")
+    public ResponseEntity<Object> loadSettings() {
+        ApiResponse response = new ApiResponse<>();
+        try {
+            List<Settings> savedSettings = settingsRepository.findAll();
+            response.setEntity(savedSettings);
+            response.setMessage("Found");
+            response.setStatusCode(200);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println("Error "+ e);
+            return null;
+        }
+    }
 
 
 
